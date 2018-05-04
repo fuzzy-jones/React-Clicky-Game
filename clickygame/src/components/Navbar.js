7cb6893acc30441688dc93dfe6f690ce3c01feba
FuzzyJones <donald.jones83@gmail.com>
2018-05-04 18:50:25 -0400

    static navbar

--- clickygame/src/components/Navbar.js
+++ clickygame/src/components/Navbar.js
@@ -5,7 +5,8 @@ import "../styles/Navbar.css";
 const Navbar = () => (
         <div className="navbar">
             <a className="navbar-brand" href="/">Clicky</a>
-            <a id="navcenter">Click any image to begin</a>
+            <li id="navcenter">Click any image to begin</li>
+            <li id="score">Score: 0 | Top Score: 0</li>
         </div>
     
 
